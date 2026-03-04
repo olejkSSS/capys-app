a365078d4604c2a0a9421b7dfc9202ccf49e357c
olejKSSS <discordercrow@gmail.com>
2026-03-04 18:27:40 +0200

    animated background blobs

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,19 +24,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-  className={`${geistSans.variable} ${geistMono.variable} antialiased relative min-h-screen bg-[#020617] text-white overflow-x-hidden`}
->
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
 
-  {/* animated background */}
-  <div className="pointer-events-none fixed inset-0 -z-10 overflow-hidden">
-
-    <div className="absolute top-[-200px] left-[-200px] w-[500px] h-[500px] bg-purple-600/20 blur-[120px] rounded-full animate-blob"></div>
-
-    <div className="absolute bottom-[-200px] right-[-200px] w-[500px] h-[500px] bg-cyan-500/20 blur-[120px] rounded-full animate-blob animation-delay-2000"></div>
-
-    <div className="absolute top-[40%] left-[60%] w-[400px] h-[400px] bg-pink-500/20 blur-[120px] rounded-full animate-blob animation-delay-4000"></div>
-
+  <div className="fixed inset-0 -z-10 overflow-hidden">
+    <div className="absolute top-0 left-0 w-96 h-96 bg-purple-600 rounded-full mix-blend-multiply filter blur-3xl opacity-20 animate-blob"></div>
+    <div className="absolute top-0 right-0 w-96 h-96 bg-blue-600 rounded-full mix-blend-multiply filter blur-3xl opacity-20 animate-blob animation-delay-2000"></div>
+    <div className="absolute bottom-0 left-1/2 w-96 h-96 bg-pink-600 rounded-full mix-blend-multiply filter blur-3xl opacity-20 animate-blob animation-delay-4000"></div>
   </div>
 
   {children}

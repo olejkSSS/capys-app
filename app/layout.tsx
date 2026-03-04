dd631a420fb35952addafb0cf9ee81858e8a51e6
olejKSSS <discordercrow@gmail.com>
2026-03-04 18:44:35 +0200

    update

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,17 +24,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+  <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
 
-  <div className="fixed inset-0 -z-10 overflow-hidden">
-    <div className="absolute top-0 left-0 w-96 h-96 bg-purple-600 rounded-full mix-blend-multiply filter blur-3xl opacity-20 animate-blob"></div>
-    <div className="absolute top-0 right-0 w-96 h-96 bg-blue-600 rounded-full mix-blend-multiply filter blur-3xl opacity-20 animate-blob animation-delay-2000"></div>
-    <div className="absolute bottom-0 left-1/2 w-96 h-96 bg-pink-600 rounded-full mix-blend-multiply filter blur-3xl opacity-20 animate-blob animation-delay-4000"></div>
-  </div>
 
-  {children}
 
-</body>
-    </html>
+    {children}
+
+  </body>
+</html>
   );
 }

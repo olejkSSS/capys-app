2ff8fca357d0e1f6cc32c729934833076338f085
olejKSSS <discordercrow@gmail.com>
2026-03-23 20:31:25 +0200

    miniiifix

--- app/page.tsx
+++ app/page.tsx
@@ -1315,7 +1315,7 @@ Calculate yours on capys.app`
             </p>
 
             <div className="mt-3 inline-flex rounded-full border border-fuchsia-400/20 bg-fuchsia-400/10 px-3 py-1 text-[11px] uppercase tracking-[0.22em] text-fuchsia-300/80">
-              Last updated: Mar 12, 9:00 UTC
+              Last updated: Mar 23, 12:00 UTC
             </div>
           </div>
 
